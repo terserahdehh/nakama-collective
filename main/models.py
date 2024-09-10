12cd05c12a2e94eb440fc7be334b6c3dcf77021b
NALA <serafina.setiawan@cs.ui.ac.id>
2024-09-10 17:31:15 +0700

    updated

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.Charfield(max_length = 255)
+    name = models.CharField(max_length = 255)
     price = models.IntegerField()
     description = models.TextField()
