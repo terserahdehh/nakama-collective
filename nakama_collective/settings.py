c17bb792784950c08111945e18d6aab2218bcb69
NALA <serafina.setiawan@cs.ui.ac.id>
2024-10-02 09:33:55 +0700

    add github actions-PWS

--- nakama_collective/settings.py
+++ nakama_collective/settings.py
@@ -131,4 +131,4 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://serafina-nala-nakamacollective.pbp.cs.ui.ac.id/", "http://serafina-nala-nakamacollective.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://serafina-nala-nakamacollective.pbp.cs.ui.ac.id/", "https://serafina-nala-nakamacollective.pbp.cs.ui.ac.id/"]
